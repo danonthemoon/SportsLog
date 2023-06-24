0b5de14eb51a086191eb5b69c41925f4e659f3b3
nbgitpuller <nbgitpuller@example.com>
2023-06-24 10:59:35 -0700

    MLB/getStats cleanup

--- MLB/getStats.py
+++ MLB/getStats.py
@@ -10,6 +10,20 @@ import sqlite3
 from sqlite3 import Error
 import sys
 
+def usage():
+    print()
+    print('-------------------------------------------------------------------------------')
+    print("  getStats.py is a script to get box score data from a specificed MLB game")
+    print('-------------------------------------------------------------------------------')
+    print()
+    print("Usage:   getStats.py TEAMABBREV DATE")
+    print()
+    print()
+    print("Example: getStats.py SDP 20221001")
+    print()
+    sys.exit(0)
+
+
 
 gameDate, startTime, attendance, stadium, gameDur = " " * 5
 teams = { 'CHC': 'CHN',
@@ -85,13 +99,89 @@ pitchingNames = {"ATL":"AtlantaBravespitching",
                     "TOR":"TorontoBlueJayspitching",
                     "WAS":"WashingtonNationalspitching"}
 
+def batting(team,soup):
+    teamBatting = soup.find_all("table", {"id":"%s" % battingNames[team]})
+    data_rows = teamBatting[0].findAll('tr')
+    data_header = teamBatting[0].findAll('thead')
+    data_header = data_header[0].findAll("tr")
+    data_header = data_header[0].findAll("th")
+    game_data = [[td.getText() for td in data_rows[i].findAll(['th','td'])]
+        for i in range(len(data_rows))
+        ]
+    data = pd.DataFrame(game_data)
+    header = []
+    for i in range(len(data.columns)):
+        header.append(data_header[i].getText())
+    data.columns = header
+    data = data.loc[data[header[0]] != header[0]]
+    data = data.reset_index(drop = True)
+    data.drop(columns=['BA','OBP','SLG','OPS','Pit','Str','WPA','WPA+','WPA-','cWPA','aLI','acLI','RE24','PO','A'],inplace=True)
+    data = data[data.PA != '']
+    return data
+
+def pitching(team,soup):
+    teamPitching = soup.find_all("table", {"id":"%s" % pitchingNames[team]})
+    data_rows = teamPitching[0].findAll('tr')
+    data_header = teamPitching[0].findAll('thead')
+    data_header = data_header[0].findAll("tr")
+    data_header = data_header[0].findAll("th")
+    game_data = [[td.getText() for td in data_rows[i].findAll(['th','td'])]
+        for i in range(len(data_rows))
+        ]
+    data = pd.DataFrame(game_data)
+    header = []
+    for i in range(len(data.columns)):
+        header.append(data_header[i].getText())
+    data.columns = header
+    data = data.loc[data[header[0]] != header[0]]
+    data = data.reset_index(drop = True)
+    data.drop(columns=['Ctct','StS','StL','GB','FB','LD','Unk','GSc','IR','IS','WPA','aLI','cWPA','acLI','RE24'],inplace=True)
+    return data
+
+
+def output(hteam, ateam, gameInfo, homeBatting, awayBatting, homePitching, awayPitching):
+    print()
+    print('-------------------')
+    print('    ' + hteam + ' vs ' + ateam)
+    print('-------------------')
+    print()
+    for i in gameInfo:
+        print(i)
+    print()
+    print('------------------------------------------------------------')
+    print('                         ' + hteam + ' Batting')
+    print('------------------------------------------------------------')
+    print(homeBatting)
+    print()
+    print('------------------------------------------------------------')
+    print('                         ' + ateam + ' Batting')
+    print('------------------------------------------------------------')
+    print(awayBatting)
+    print()
+    print('------------------------------------------------------------------------')
+    print('                         ' + hteam + ' Pitching')
+    print('------------------------------------------------------------------------')
+    print(homePitching)
+    print()
+    print('------------------------------------------------------------------------')
+    print('                         ' + ateam + ' Pitching')
+    print('------------------------------------------------------------------------')
+    print(awayPitching)
+    print()
+
 def main():
-    hteam = sys.argv[1]
-    gameDate = sys.argv[2]
+    # TODO make this more robust
+    try:
+        hteam = sys.argv[1]
+        gameDate = sys.argv[2]
+    except:
+        usage()
+
     homeTeam=hteam
     if homeTeam in teams.keys():
         homeTeam = teams[homeTeam]
 
+    # TODO account for doubleheaders
     url = 'https://www.baseball-reference.com/boxes/%s/%s%s0.shtml' % (homeTeam,homeTeam,gameDate)
     res = requests.get(url)
     comm = re.compile("<!--|-->")
@@ -99,14 +189,11 @@ def main():
 
     scorebox = soup.find(attrs = {'class' : 'scorebox'})
     teamLinks=scorebox.select("a[href*=teams]")
+
     ateam = (teamLinks[0]['href'].split('/')[2])
     awayTeam=ateam
     if awayTeam in teams.keys():
         awayTeam = teams[awayTeam]
-
-    print()
-    print('    ' + hteam + ' vs ' + ateam)
-    print('------------------------')
 
     gameMetadata = soup.find(attrs = {'class' : 'scorebox_meta'})
     gameMetadataList = gameMetadata.find_all('div')
@@ -119,104 +206,15 @@ def main():
     #attendance = int(divs[2].text.strip("Attendance: ").replace(',',''))
     #stadium = divs[3].text.strip("Venue: ")
     #gameDur = divs[4].text.strip("Game Duration: ")
+
     gameInfo = [gameDate, startTime, attendance, stadium, gameDur]
-    for i in gameInfo:
-        print(i)
+    homeBatting = batting(homeTeam, soup)
+    awayBatting = batting(awayTeam, soup)
+    homePitching = pitching(homeTeam, soup)
+    awayPitching = pitching(awayTeam, soup)
 
-    print()
-    print('------------------------------------------------------------')
-    print('                         ' + hteam + ' Batting')
-    print('------------------------------------------------------------')
-    homeBatting = soup.find_all("table", {"id":"%s" % battingNames[homeTeam]})
-    data_rows = homeBatting[0].findAll('tr')
-    data_header = homeBatting[0].findAll('thead')
-    data_header = data_header[0].findAll("tr")
-    data_header = data_header[0].findAll("th")
-    game_data = [[td.getText() for td in data_rows[i].findAll(['th','td'])]
-        for i in range(len(data_rows))
-        ]
-    data = pd.DataFrame(game_data)
-    header = []
-    for i in range(len(data.columns)):
-        header.append(data_header[i].getText())
-    data.columns = header
-    data = data.loc[data[header[0]] != header[0]]
-    data = data.reset_index(drop = True)
-    data.drop(columns=['BA','OBP','SLG','OPS','Pit','Str','WPA','WPA+','WPA-','cWPA','aLI','acLI','RE24','PO','A'],inplace=True)
-    data = data[data.PA != '']
-    print(data)
-
-
-    print()
-    print('------------------------------------------------------------')
-    print('                         ' + ateam + ' Batting')
-    print('------------------------------------------------------------')
-    awayBatting = soup.find_all("table", {"id":"%s" % battingNames[awayTeam]})
-    data_rows = awayBatting[0].findAll('tr')
-    data_header = awayBatting[0].findAll('thead')
-    data_header = data_header[0].findAll("tr")
-    data_header = data_header[0].findAll("th")
-    game_data = [[td.getText() for td in data_rows[i].findAll(['th','td'])]
-        for i in range(len(data_rows))
-        ]
-    data = pd.DataFrame(game_data)
-    header = []
-    for i in range(len(data.columns)):
-        header.append(data_header[i].getText())
-    data.columns = header
-    data = data.loc[data[header[0]] != header[0]]
-    data = data.reset_index(drop = True)
-    data.drop(columns=['BA','OBP','SLG','OPS','Pit','Str','WPA','WPA+','WPA-','cWPA','aLI','acLI','RE24','PO','A'],inplace=True)
-    data = data[data.PA != '']
-    print(data)
-
-
-    print()
-    print('------------------------------------------------------------------------')
-    print('                         ' + hteam + ' Pitching')
-    print('------------------------------------------------------------------------')
-    homePitching = soup.find_all("table", {"id":"%s" % pitchingNames[homeTeam]})
-    data_rows = homePitching[0].findAll('tr')
-    data_header = homePitching[0].findAll('thead')
-    data_header = data_header[0].findAll("tr")
-    data_header = data_header[0].findAll("th")
-    game_data = [[td.getText() for td in data_rows[i].findAll(['th','td'])]
-        for i in range(len(data_rows))
-        ]
-    data = pd.DataFrame(game_data)
-    header = []
-    for i in range(len(data.columns)):
-        header.append(data_header[i].getText())
-    data.columns = header
-    data = data.loc[data[header[0]] != header[0]]
-    data = data.reset_index(drop = True)
-    data.drop(columns=['Ctct','StS','StL','GB','FB','LD','Unk','GSc','IR','IS','WPA','aLI','cWPA','acLI','RE24'],inplace=True)
-    print(data)
-
-
-    print()
-    print('------------------------------------------------------------------------')
-    print('                         ' + ateam + ' Pitching')
-    print('------------------------------------------------------------------------')
-    awayPitching = soup.find_all("table", {"id":"%s" % pitchingNames[awayTeam]})
-    data_rows = awayPitching[0].findAll('tr')
-    data_header = awayPitching[0].findAll('thead')
-    data_header = data_header[0].findAll("tr")
-    data_header = data_header[0].findAll("th")
-    game_data = [[td.getText() for td in data_rows[i].findAll(['th','td'])]
-        for i in range(len(data_rows))
-        ]
-    data = pd.DataFrame(game_data)
-    header = []
-    for i in range(len(data.columns)):
-        header.append(data_header[i].getText())
-    data.columns = header
-    data = data.loc[data[header[0]] != header[0]]
-    data = data.reset_index(drop = True)
-    data.drop(columns=['Ctct','StS','StL','GB','FB','LD','Unk','GSc','IR','IS','WPA','aLI','cWPA','acLI','RE24'],inplace=True)
-    print(data)
-
-    print()
+    output(hteam, ateam, gameInfo, homeBatting, awayBatting, homePitching, awayPitching)
 
 if __name__ == "__main__":
+
    main()
